ac2cdac35ef3c9c727e576d5dc819fbacff6208b
sunabove <sunabove@users.noreply.github.com>
2019-10-04 19:14:54 +0900

    **

---
+++ led_pwm.py
@@ -0,0 +1,17 @@
+from gpiozero import PWMLED
+from time import sleep
+
+pwmLed = PWMLED(17)
+
+while True:
+   for x in range( 101 ) :
+      pwmLed.value = x*0.01
+      sleep(0.02)
+
+   sleep( 0.3 )
+
+   for x in range( 101 ) :
+      pwmLed.value = (100 - x)*0.01
+      sleep(0.02)
+
+   sleep( 0.3 )

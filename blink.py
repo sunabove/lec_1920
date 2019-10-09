c4cdf06429db2f5dfbcf009babb2a622b5b9ef12
sunabove <sunabove@users.noreply.github.com>
2019-10-09 12:15:32 +0900

    **

---
+++ blink.py
@@ -0,0 +1,16 @@
+# coding: utf-8
+from gpiozero import LED
+from time import sleep
+
+led = LED(17)
+print( "Press Ctrl + C to quit! ")
+while True:
+	led.on()
+	print("+", end = '', flush=True )
+	sleep(1)
+	led.off()
+	print("\b-", end = '', flush=True)
+	sleep(1)
+	print("\b", end = '', flush=True)
+pass
+

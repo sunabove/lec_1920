c5154dba7e59f98436542cdafe513b34c06d8342
sunabove <sunabove@users.noreply.github.com>
2019-11-09 17:00:19 +0900

    **

--- layout/gui_car.py
+++ layout/gui_car.py
@@ -1,24 +1,4 @@
 # coding: utf-8
-def check_pkg( pkg ) : 
-	try:
-		import importlib
-		importlib.import_module( pkg )
-	except ModuleNotFoundError :
-		print( '%s package is not installed, installing it now ...' % pkg )
-		import sys 
-		try:
-			from pip import main as pipmain
-		except:
-			from pip._internal import main as pipmain
-		pass
-		pipmain( ['install', pkg ] )
-	pass
-pass
-
-for pkg in [ "guizero" ] :
-	check_pkg( pkg )
-pass
-
 from guizero import *
 
 #picture1 = Picture(app, image="std1.gif", grid=[0,0] )

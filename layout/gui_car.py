5be4fc0301d6726e2be2315b057d8aaa2dbdbdd7
sunabove <sunabove@nate.com>
2019-11-14 05:44:31 +0000

    **

--- layout/gui_car.py
+++ layout/gui_car.py
@@ -12,31 +12,36 @@ options_box = Box(app, height="fill", align="right", border=True)
 options = Text(options_box, text="options")
 
 content_box = Box(app, align="top", width="fill", height="fill", border=True)
-Text(content_box, text="content", height="fill")
+pic_box = Box(content_box, width="fill", height="fill", border=True)
+picture = Picture(pic_box, image="std1.gif" ) 
 
-box = Box(content_box, width="fill", height="fill", border=True)
-form_box = Box(box, layout="grid", border=True)
+#box = Box(content_box, width="fill", border=True)
+control_box = Box(content_box, layout="grid", border=True)
 
-font="Verdana 19 bold"
+font="Verdana 22 bold"
 
-btn = Text(form_box, grid=[0,0], text="  " )
-btn = PushButton(form_box, grid=[1,0], text=" ↑ " )
+btn = Text(control_box, grid=[0,0], text="  " )
+btn = PushButton(control_box, grid=[1,0], text=" ↑ " )
 btn.font = font
-btn = Text(form_box, grid=[2,0], text="  " )
+btn = Text(control_box, grid=[2,0], text="  " )
 
-btn = PushButton(form_box, grid=[0,1], text="←" )
+btn = PushButton(control_box, grid=[0,1], text="←" )
 btn.font = font
-btn = PushButton(form_box, grid=[1,1], text=" * " )
-btn.font = "Verdana 21 bold"
-btn = PushButton(form_box, grid=[2,1], text="→" )
+btn = PushButton(control_box, grid=[1,1], text=" * " )
+btn.font = font
+btn = PushButton(control_box, grid=[2,1], text="→" )
 btn.font = font
 
-btn = Text(form_box, grid=[0,2], text="  " )
-btn = PushButton(form_box, grid=[1,2], text=" ↓ " )
+btn = Text(control_box, grid=[0,2], text="  " )
+btn = PushButton(control_box, grid=[1,2], text=" ↓ " )
 btn.font = font
-btn = Text(form_box, grid=[2,2], text="  " )
+btn = Text(control_box, grid=[2,2], text="  " )
 
 buttons_box = Box(app, width="fill", align="bottom", border=True)
 status = Text(buttons_box, text="status", align="left")
 
 app.display()
+
+print( "Hello...." )
+
+pass

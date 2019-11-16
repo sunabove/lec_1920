c64dc8936f4134398910853caae23a4d9639e5eb
sunabove <sunabove@nate.com>
2019-11-16 01:59:55 +0000

    **

--- html/h01_simple.py
+++ html/h01_simple.py
@@ -7,12 +7,43 @@ from threading import Condition
 from http import server
 
 page_index="""\
+<!DOCTYPE html>
 <html>
 <head>
-<title>My First Html Page</title>
+<style>
+table, th, td {
+  border: 2px dotted red;
+}
+</style>
 </head>
 <body>
-<p>Hello World!</p>
+
+<h2>Bordered Table</h2>
+<p>Use the CSS border property to add a border to the table.</p>
+
+<table style="width:100%">
+  <tr>
+    <th>Firstname</th>
+    <th>Lastname</th> 
+    <th>Age</th>
+  </tr>
+  <tr>
+    <td>Jill</td>
+    <td>Smith</td>
+    <td>50</td>
+  </tr>
+  <tr>
+    <td>Eve</td>
+    <td>Jackson</td>
+    <td>94</td>
+  </tr>
+  <tr>
+    <td>John</td>
+    <td>Doe</td>
+    <td>80</td>
+  </tr>
+</table>
+
 </body>
 </html>
 """

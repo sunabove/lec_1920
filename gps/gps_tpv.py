ea435b64299d4a10944d25187428b0cdb54ea596
sunabove <sunabove@nate.com>
2019-11-26 06:05:27 +0000

    **

--- gps/gps_tpv.py
+++ gps/gps_tpv.py
@@ -7,12 +7,9 @@ gpsd = gps(mode=WATCH_ENABLE|WATCH_NEWSTYLE)
 print ( 'latitude\tlongitude\ttime utc\t\t\taltitude\tepv\tept\tspeed\tclimb' )
    
 try:
- 
- 
     while True:
         report = gpsd.next() #
-        if report['class'] == 'TPV':
-             
+        if report['class'] == 'TPV':             
             print( getattr(report,'lat',0.0),"\t" )
             print( getattr(report,'lon',0.0),"\t" )
             print( getattr(report,'time',''),"\t" )
@@ -21,9 +18,11 @@ try:
             print(  getattr(report,'ept','nan'),"\t" )
             print(  getattr(report,'speed','nan'),"\t" )
             print( getattr(report,'climb','nan'),"\t" )
- 
-        time.sleep(1) 
+        pass 
+        time.sleep(1)
+    pass
  
 except (KeyboardInterrupt, SystemExit): #when you press ctrl+c
     print( "Done.\nExiting." )
+pass
 # end

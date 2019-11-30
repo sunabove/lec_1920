c181f6348a108a8bd35024fd61aba157b6e76fa0
sunabove <sunabove@nate.com>
2019-11-30 05:41:17 +0000

    **

--- gps/gps_parse.py
+++ gps/gps_parse.py
@@ -1,5 +1,4 @@
 # coding: utf-8
-
 def check_pkg( pkg ) : 
 	try:
 		import importlib
@@ -17,49 +16,43 @@ def check_pkg( pkg ) :
 	pass
 pass
 
-for pkg in [ "serial, pyserial" ] :
+for pkg in [ "serial, pyserial", "pynmea2" ] :
 	check_pkg( pkg )
 pass
 
-def parseGPS(data): #    print "raw:", data #prints raw data
-    if data[0:6] in ( "$GPRMC", "$GNRMC" ) :
-        sdata = data.split(",")
-        if sdata[2] == 'V':
-            print( "no satellite data available" )
-            return
+
+
+import serial
+import pynmea2
+
+gps_parse_cnt = 0 
+def parseGPS(str):
+    if 'GGA' in str :
+        global gps_parse_cnt
+        gps_parse_cnt += 1
+        msg = pynmea2.parse(str)
+        print( "[%04d] %s" % ( gps_parse_cnt, str ) , end ="" )
+        print( "[%04d] Timestamp: %s -- Lat: %s %s -- Lon: %s %s -- Altitude: %s %s -- Satellites: %s" % ( gps_parse_cnt, msg.timestamp,msg.lat,msg.lat_dir,msg.lon,msg.lon_dir,msg.altitude,msg.altitude_units,msg.num_sats) )
+    pass
+pass 
+
+def read_gps() :
+    while 1 :  
+        try : 
+            gps_serial = serial.Serial("/dev/serial0", baudrate = 9600, timeout = 20 )
+            while 1 :
+                str = gps_serial.readline()
+                parseGPS(str.decode( "utf-8"))
+            pass
+        except Exception as e:
+            #print( str(e) )
+            print( "Serial Device Error" )
+            import time
+            time.sleep( 3 )
         pass
-    
-        print( "---Parsing GPRMC---" )
-        time = sdata[1][0:2] + ":" + sdata[1][2:4] + ":" + sdata[1][4:6]
-        lat = decode(sdata[3]) #latitude
-        dirLat = sdata[4]      #latitude direction N/S
-        lon = decode(sdata[5]) #longitute
-        dirLon = sdata[6]      #longitude direction E/W
-        speed = sdata[7]       #Speed in knots
-        trCourse = sdata[8]    #True course
-        date = sdata[9][0:2] + "/" + sdata[9][2:4] + "/" + sdata[9][4:6]#date
- 
-        print( "time : %s, latitude : %s(%s), longitude : %s(%s), speed : %s, True Course : %s, Date : %s" % (time,lat,dirLat,lon,dirLon,speed,trCourse,date) )
     pass
 pass
- 
-def decode(coord):
-    #Converts DDDMM.MMMMM > DD deg MM.MMMMM min
-    x = coord.split(".")
-    head = x[0]
-    tail = x[1]
-    deg = head[0:-2]
-    min = head[-2:]
-    return deg + " deg " + min + "." + tail + " min"
- 
- 
-print( "Receiving GPS data" )
-import serial
-ser = serial.Serial( "/dev/serial0", baudrate = 9600, timeout = 0.5)
-count = 1
-while 1 :
-   data = ser.readline().decode( "utf-8")
-   #print( "[%04d] %s" % ( count, data ) )
-   count += 1
-   parseGPS(data)
+
+if __name__ == "__main__" : 
+    read_gps()
 pass

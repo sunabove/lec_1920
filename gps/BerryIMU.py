28c4a387156ddf68c62cee7692eb46c9d0003e43
sunabove <sunabove@nate.com>
2019-12-03 02:10:32 +0900

    **

--- gps/BerryIMU.py
+++ gps/BerryIMU.py
@@ -66,82 +66,9 @@ KFangleY = 0.0
 
 
 
-def kalmanFilterY ( accAngle, gyroRate, DT):
-	y=0.0
-	S=0.0
-
-	global KFangleY
-	global Q_angle
-	global Q_gyro
-	global y_bias
-	global YP_00
-	global YP_01
-	global YP_10
-	global YP_11
-
-	KFangleY = KFangleY + DT * (gyroRate - y_bias)
-
-	YP_00 = YP_00 + ( - DT * (YP_10 + YP_01) + Q_angle * DT )
-	YP_01 = YP_01 + ( - DT * YP_11 )
-	YP_10 = YP_10 + ( - DT * YP_11 )
-	YP_11 = YP_11 + ( + Q_gyro * DT )
-
-	y = accAngle - KFangleY
-	S = YP_00 + R_angle
-	K_0 = YP_00 / S
-	K_1 = YP_10 / S
-	
-	KFangleY = KFangleY + ( K_0 * y )
-	y_bias = y_bias + ( K_1 * y )
-	
-	YP_00 = YP_00 - ( K_0 * YP_00 )
-	YP_01 = YP_01 - ( K_0 * YP_01 )
-	YP_10 = YP_10 - ( K_1 * YP_00 )
-	YP_11 = YP_11 - ( K_1 * YP_01 )
-	
-	return KFangleY
-
-def kalmanFilterX ( accAngle, gyroRate, DT):
-	x=0.0
-	S=0.0
-
-	global KFangleX
-	global Q_angle
-	global Q_gyro
-	global x_bias
-	global XP_00
-	global XP_01
-	global XP_10
-	global XP_11
-
-
-	KFangleX = KFangleX + DT * (gyroRate - x_bias)
-
-	XP_00 = XP_00 + ( - DT * (XP_10 + XP_01) + Q_angle * DT )
-	XP_01 = XP_01 + ( - DT * XP_11 )
-	XP_10 = XP_10 + ( - DT * XP_11 )
-	XP_11 = XP_11 + ( + Q_gyro * DT )
-
-	x = accAngle - KFangleX
-	S = XP_00 + R_angle
-	K_0 = XP_00 / S
-	K_1 = XP_10 / S
-	
-	KFangleX = KFangleX + ( K_0 * x )
-	x_bias = x_bias + ( K_1 * x )
-	
-	XP_00 = XP_00 - ( K_0 * XP_00 )
-	XP_01 = XP_01 - ( K_0 * XP_01 )
-	XP_10 = XP_10 - ( K_1 * XP_00 )
-	XP_11 = XP_11 - ( K_1 * XP_01 )
-	
-	return KFangleX
-pass
-
-
 class BerryIMU : 
     def __init__( self ) :
-        self.dbg = 1
+        self.dbg = 0
         self.use_curses = 0
         self.init_curses = 0 
 
@@ -164,6 +91,81 @@ class BerryIMU :
             curses.endwin() 
         pass
     pass
+
+    def kalmanFilterY ( self, accAngle, gyroRate, DT):
+        y=0.0
+        S=0.0
+
+        global KFangleY
+        global Q_angle
+        global Q_gyro
+        global y_bias
+        global YP_00
+        global YP_01
+        global YP_10
+        global YP_11
+
+        KFangleY = KFangleY + DT * (gyroRate - y_bias)
+
+        YP_00 = YP_00 + ( - DT * (YP_10 + YP_01) + Q_angle * DT )
+        YP_01 = YP_01 + ( - DT * YP_11 )
+        YP_10 = YP_10 + ( - DT * YP_11 )
+        YP_11 = YP_11 + ( + Q_gyro * DT )
+
+        y = accAngle - KFangleY
+        S = YP_00 + R_angle
+        K_0 = YP_00 / S
+        K_1 = YP_10 / S
+        
+        KFangleY = KFangleY + ( K_0 * y )
+        y_bias = y_bias + ( K_1 * y )
+        
+        YP_00 = YP_00 - ( K_0 * YP_00 )
+        YP_01 = YP_01 - ( K_0 * YP_01 )
+        YP_10 = YP_10 - ( K_1 * YP_00 )
+        YP_11 = YP_11 - ( K_1 * YP_01 )
+        
+        return KFangleY
+
+    def kalmanFilterX (self, accAngle, gyroRate, DT):
+        x=0.0
+        S=0.0
+
+        global KFangleX
+        global Q_angle
+        global Q_gyro
+        global x_bias
+        global XP_00
+        global XP_01
+        global XP_10
+        global XP_11
+
+
+        KFangleX = KFangleX + DT * (gyroRate - x_bias)
+
+        XP_00 = XP_00 + ( - DT * (XP_10 + XP_01) + Q_angle * DT )
+        XP_01 = XP_01 + ( - DT * XP_11 )
+        XP_10 = XP_10 + ( - DT * XP_11 )
+        XP_11 = XP_11 + ( + Q_gyro * DT )
+
+        x = accAngle - KFangleX
+        S = XP_00 + R_angle
+        K_0 = XP_00 / S
+        K_1 = XP_10 / S
+        
+        KFangleX = KFangleX + ( K_0 * x )
+        x_bias = x_bias + ( K_1 * x )
+        
+        XP_00 = XP_00 - ( K_0 * XP_00 )
+        XP_01 = XP_01 - ( K_0 * XP_01 )
+        XP_10 = XP_10 - ( K_1 * XP_00 )
+        XP_11 = XP_11 - ( K_1 * XP_01 )
+        
+        return KFangleX
+    pass
+
+
+
 
     def get_imu_data( self ) : 
         dbg = self.dbg
@@ -348,8 +350,8 @@ class BerryIMU :
             CFangleY=AA*(CFangleY+rate_gyr_y*LP) +(1 - AA) * AccYangle
 
             #Kalman filter used to combine the accelerometer and gyro values.
-            kalmanY = kalmanFilterY(AccYangle, rate_gyr_y,LP)
-            kalmanX = kalmanFilterX(AccXangle, rate_gyr_x,LP)
+            kalmanY = self.kalmanFilterY(AccYangle, rate_gyr_y,LP)
+            kalmanX = self.kalmanFilterX(AccXangle, rate_gyr_x,LP)
 
             if IMU_UPSIDE_DOWN:
                 MAGy = -MAGy      #If IMU is upside down, this is needed to get correct heading.
@@ -393,6 +395,7 @@ class BerryIMU :
                 magYcomp = MAGx*math.sin(roll)*math.sin(pitch)+MAGy*math.cos(roll)-MAGz*math.sin(roll)*math.cos(pitch)   #LSM9DS0
             else:
                 magYcomp = MAGx*math.sin(roll)*math.sin(pitch)+MAGy*math.cos(roll)+MAGz*math.sin(roll)*math.cos(pitch)   #LSM9DS1
+            pass
 
 
 
@@ -486,6 +489,7 @@ pass
 if __name__ == '__main__':
     berryIMU = BerryIMU()
     berryIMU.use_curses = 1
+    berryIMU.dbg = 1
     
     try : 
         berryIMU.get_imu_data()

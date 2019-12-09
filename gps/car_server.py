b4a1b53b05d97519c89e015812650446948cdac9
sunabove <sunabove@nate.com>
2019-12-10 07:12:03 +0900

    **

--- gps/car_server.py
+++ gps/car_server.py
@@ -468,6 +468,7 @@ pass
 # web by flask framewwork
 from flask import Flask, render_template, Response, jsonify
 from flask import request 
+from flask_socketio import SocketIO, emit
 
 from BerryIMU import *
 
@@ -502,6 +503,8 @@ def init_system() :
 pass
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'secret!'
+socketio = SocketIO(app)
 
 def gen(camera):
     global ads 
@@ -561,11 +564,35 @@ pass
 
 # -- web by flask framewwork
 
+# socket io
+
+@socketio.on('my event', namespace='/test')
+def test_message_01(message):
+    emit('my response', {'data': message['data']})
+
+@socketio.on('my broadcast event', namespace='/test')
+def test_messag_02(message):
+    emit('my response', {'data': message['data']}, broadcast=True)
+
+@socketio.on('connect', namespace='/test')
+def test_connect():
+    emit('my response', {'data': 'Connected'})
+
+@socketio.on('disconnect', namespace='/test')
+def test_disconnect():
+    print('Client disconnected') 
+
+# -- socket io
+
 if __name__ == '__main__':
     use_ssl = 0
     if use_ssl : 
         app.run(host='0.0.0.0', port=443, ssl_context='adhoc', debug=True)
+
+        socketio.run(app)
     else :
         app.run(host='0.0.0.0', port=80, debug=True)
+
+        socketio.run(app)
     pass
 pass

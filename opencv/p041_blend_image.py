e2fd85e4ec0b83f1d00eab494c033b9e9b435fb3
sunabove <sunabove@nate.com>
2019-12-22 11:55:03 +0900

    **

--- opencv/p041_blend_image.py
+++ opencv/p041_blend_image.py
@@ -3,7 +3,9 @@
 import cv2
 
 img1 = cv2.imread('ml.png')
-img2 = cv2.imread('opencv_logo.jpg')
+img2 = cv2.imread('opencv_logo.png')
+height, width, _ = img2.shape
+img2 = cv2.resize(img2, (width, height))
 dst = cv2.addWeighted(img1,0.7,img2,0.3,0)
 cv2.imshow('dst',dst)
 cv2.waitKey(0)

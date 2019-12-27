8a0835311ee28eeabf98ab4f2b2184d44edf8cab
sunabove <sunabove@nate.com>
2019-12-27 21:45:20 +0000

    **

--- servo.py
+++ servo.py
@@ -7,14 +7,14 @@ servo = Servo(4)
 while True:
    print( "servo min")
    servo.min()
-   sleep(5)
+   sleep(1)
    print( "servo mid")
    servo.mid()
-   sleep(5)
+   sleep(1)
    print( "servo max")
    servo.max()
-   sleep(5)
+   sleep(1)
    print( "servo value 0.5")
    servo.value = 0.5
-   sleep(5)
+   sleep(1)
 pass

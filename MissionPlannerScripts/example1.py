9c2769bbb81d6476cceb60c8c1c425afe37f8442
sunabove <sunabove@users.noreply.github.com>
2020-01-28 12:19:02 +0900

    Update example1.py

--- MissionPlannerScripts/example1.py
+++ MissionPlannerScripts/example1.py
@@ -9,71 +9,33 @@
 # 
 
 print 'Start Script'
-Script.SendRC(3,1550,True) # throttle
 
 for chan in range(1,9):
     Script.SendRC(chan,1500,False)
 Script.SendRC(3,Script.GetParam('RC3_MIN'),True)
 
 Script.Sleep(5000)
-while cs.lat == 0:
-    print 'Waiting for GPS'
-    Script.Sleep(1000)
-pass
 
-print 'Got GPS'
-jo = 10 * 13
-print jo
-Script.SendRC(3,1000,False)
-Script.SendRC(4,2000,True)
+
+Script.SendRC(3,1500,False)
+Script.SendRC(4,1982,True)
 cs.messages.Clear()
-Script.WaitFor('ARMING MOTORS',30000)
-Script.SendRC(4,1500,True)
+print 'ARMING MOTORS' 
+Script.SendRC(4,1982,True)
+Script.Sleep(5000)  
 print 'Motors Armed!'
 
-Script.SendRC(3,1700,True)
+Script.SendRC(3,1900,True) # throttle
 
-if 0 : 
-    while cs.alt < 50:
-        Script.Sleep(50)
-    pass
-pass
+Script.Sleep(2000)  
 
-if 0: 
-    Script.SendRC(5,2000,True) # acro
+Script.SendRC(3,1400,True) # throttle back to land
 
-    Script.SendRC(1,2000,False) # roll
-    Script.SendRC(3,1370,True) # throttle
-pass
-
-Script.SendRC(3,1500,True) # throttle
-
-if 0 : 
-    while cs.roll > -45: # top hald 0 - 180
-        Script.Sleep(5)
-    pass
-
-    while cs.roll < -45: # -180 - -45
-        Script.Sleep(5)
-    pass
-pass
-
-Script.SendRC(5,1500,False) # stabalise
-Script.SendRC(1,1500,True) # level roll
-Script.Sleep(2000) # 2 sec to stabalise
-Script.SendRC(3,1300,True) # throttle back to land
-
-thro = 1350 # will decend
-
-if 0 : 
-    while cs.alt > 0.1:
-        Script.Sleep(300)
-    pass
-pass
+thro = 1350 # will decend 
 
 Script.SendRC(3,1000,False)
 Script.SendRC(4,1000,True)
 Script.WaitFor('DISARMING MOTORS',30000)
 Script.SendRC(4,1500,True)
 
-print 'Roll complete'
+print 'Mission complete'

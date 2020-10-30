b9f255a184fddb02f98a011c4c77c13bcc982c02
sunabove <sunabove@nate.com>
2020-10-30 15:49:30 +0000

    **

---
+++ opencv/p064_2d_convolution.py
@@ -0,0 +1,19 @@
+# coding: utf-8
+
+import cv2
+import numpy as np
+import matplotlib as mpl
+from matplotlib import pyplot as plt
+
+mpl.rcParams['toolbar'] = 'None'
+
+img = cv2.imread('./data/opencv-logo.png') 
+
+kernel = np.ones((5,5),np.float32)/25
+dst = cv2.filter2D(img,-1,kernel)
+
+plt.subplot(121),plt.imshow(img),plt.title('Original')
+plt.xticks([]), plt.yticks([])
+plt.subplot(122),plt.imshow(dst),plt.title('Averaging')
+plt.xticks([]), plt.yticks([])
+plt.show()

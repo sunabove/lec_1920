04975e4a7b97790a45685d01a106be9fafc682ed
sunabove <sunabove@nate.com>
2021-03-24 03:45:29 +0000

    **

--- opencv/p066_median.py
+++ opencv/p066_median.py
@@ -4,9 +4,9 @@ import numpy as np
 from matplotlib import pyplot as plt
 
 img = cv2.imread('./data/opencv_logo.png')
-blur = cv2.GaussianBlur(img,(5,5), 0)
+blur = cv2.medianBlur(img, 5)
 plt.subplot(121), plt.imshow(img), plt.title('Original')
 plt.xticks([]), plt.yticks([])
-plt.subplot(122), plt.imshow(blur), plt.title('Gaussian')
+plt.subplot(122), plt.imshow(blur), plt.title('Median')
 plt.xticks([]), plt.yticks([])
 plt.show()
